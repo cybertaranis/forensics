b540d2fc614b903e99fb460c7b9fa3ffaa67d5d1
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-05-09 23:26:03 +0200

    Update current feature

--- extract.sh
+++ extract.sh
@@ -4,17 +4,18 @@ set -euxo pipefail
 
 BASE_PATH="/media/fjammes/secu"
 
+# TODO add if statement mail/file
+
 # MAILS
 REPORT_PATH="$BASE_PATH/home/LGP-mails/Reports/LGP-mails Excel Report 05-09-2023-17-26-53/Excel.csv"
 data_path="$BASE_PATH/data/mails"
 out_path="$BASE_PATH/annexes/mails"
 
-rm -rf $out_path/*
 
 # FILES
-# REPORT_PATH="$BASE_PATH/home/LGP-fichiers/Reports/LGP-fichiers Excel Report 05-09-2023-20-19-12/Excel.csv"
-# data_path="$BASE_PATH/data/"
-# OUT_PATH="$data_path/outfiles"
+REPORT_PATH="$BASE_PATH/home/LGP-fichiers/Reports/LGP-fichiers Excel Report 05-09-2023-20-19-12/Excel.csv"
+data_path="$BASE_PATH/data/"
+out_path="$BASE_PATH/annexes/fichiers"
 
 file_to_keep="/tmp/file_to_keep.txt"
 mailbody_to_keep="/tmp/mailbody_to_keep.txt"
@@ -33,9 +34,12 @@ sed -i -E "s/(.*\.zip)\/(.*)/\1/" "$file_to_keep"
 # Check mail for pdf and docx?
 
 # Mail body to keep
+# TODO add if statement
+rm "$mailbody_to_keep"
 cat "$file_to_keep" | egrep "\/([0-9])+-" | sed -E "s/(.*)\/([0-9]+)-(.*)/\1\/\2/" | uniq > $mailbody_to_keep
 
 cat $file_to_keep $mailbody_to_keep $archive_to_keep > $all_keep
 
+rm -rf $out_path/*
 rsync -av --files-from="$all_keep" "$data_path" "$out_path"
 
